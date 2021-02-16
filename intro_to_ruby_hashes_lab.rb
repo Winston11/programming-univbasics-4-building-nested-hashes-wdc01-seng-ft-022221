9d4403bcb93dad4f0218c5020afcf1d38dcf0913
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-16 23:43:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -69,7 +69,7 @@ monopoly = {
     {
       :reading_railroad=>
       {
-      "mortgage_value"=> "$100"
+      "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
@@ -81,7 +81,7 @@ monopoly = {
       },
       :shortline=>
       {
-        "mortgage_value" =>"$800"
+        "mortgage_value"=>"$800"
       }
     }
   }
